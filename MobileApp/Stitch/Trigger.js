3aaf07da08ef6ed407da63f501d4d5fcd8f6edb7
Chris Grabosky <chris@grabosky.net>
2018-11-05 10:04:06 -0500

    ignore videos

--- MobileApp/Stitch/Trigger.js
+++ MobileApp/Stitch/Trigger.js
@@ -1,17 +1,19 @@
 exports = async function(changeEvent) {
     var doc = changeEvent.fullDocument;
-    var conn = context.services.get("mongodb-atlas").db("ig").collection("media");
-    const httpService = context.services.get("cva");
-    
-    var uri = "https://eastus.api.cognitive.microsoft.com/vision/v1.0/analyze?visualFeatures=Description,Tags&language=en";
-    var data = {"url":context.values.get("azureBlobStore")+doc.path};
-    
-    var args = {"url":uri, "headers": {"Content-Type":["application/json"] , "Ocp-Apim-Subscription-Key":[context.values.get("cvaAccessKey")]}, "body":data, "encodeBodyAsJSON":true};
-    
-    var cvaresult = httpService.post(args, {"encodeBodyAsJSON":true});
-    
-    var res = await cvaresult;
-    
-    conn.updateOne({_id:doc._id},{$set: {cva: JSON.parse(res.body.text()), trigger:data.url}});
+    if(doc.type=="picture") {
+      var conn = context.services.get("mongodb-atlas").db("ig").collection("media");
+      const httpService = context.services.get("cva");
+      
+      var uri = "https://eastus.api.cognitive.microsoft.com/vision/v1.0/analyze?visualFeatures=Description,Tags&language=en";
+      var data = {"url":context.values.get("azureBlobStore")+doc.path};
+      
+      var args = {"url":uri, "headers": {"Content-Type":["application/json"] , "Ocp-Apim-Subscription-Key":[context.values.get("cvaAccessKey")]}, "body":data, "encodeBodyAsJSON":true};
+      
+      var cvaresult = httpService.post(args, {"encodeBodyAsJSON":true});
+      
+      var res = await cvaresult;
+      
+      conn.updateOne({_id:doc._id},{$set: {cva: JSON.parse(res.body.text()), trigger:data.url}});
+    }
   };
   
